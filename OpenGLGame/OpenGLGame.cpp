8009c0a598b5d3f74f14dba3679d3713314203af
Iiinnu <145962681+Iiinnu@users.noreply.github.com>
2024-01-30 15:53:18 +0100

    Shaders

--- OpenGLGame/OpenGLGame.cpp
+++ OpenGLGame/OpenGLGame.cpp
@@ -35,29 +35,14 @@ int main() {
 
     Mesh mesh2{ vertices2, size(vertices2) };
 
-    Shader vertexShader{ "#version 330 core\n"
-        "layout (location = 0) in vec3 aPos;\n"
-        "void main()\n"
-        "{\n"
-        "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-        "}\0", GL_VERTEX_SHADER };
+    Shader vertexShader{ "vertexShader.glsl", GL_VERTEX_SHADER};
 
     Shader orangeShader{
-        "#version 330 core\n"
-        "out vec4 FragColor;\n"
-        "void main()\n"
-        "{\n"
-        "    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-        "} \0", GL_FRAGMENT_SHADER
+        "orangeFragmentShader.glsl", GL_FRAGMENT_SHADER
     };
 
     Shader yellowShader{
-        "#version 330 core\n"
-        "out vec4 FragColor;\n"
-        "void main()\n"
-        "{\n"
-        "    FragColor = vec4(1.0f, 1.0f, 0.0f, 1.0f);\n"
-        "} \0", GL_FRAGMENT_SHADER
+       "yellowFragmentShader.glsl", GL_FRAGMENT_SHADER
     };
 
   
@@ -68,7 +53,7 @@ int main() {
     Triangle b{ &yellow, &mesh2 };
 
    
-    while (!window.shouldClose()) // window -> window.window
+    while (!window.shouldClose()) 
     {
         window.processInput();
 
@@ -79,7 +64,7 @@ int main() {
 
         window.present();
     }
-    // Cleans up all the GLFW stuff
+  
     glfwTerminate();
     return 0;
 }
